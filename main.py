09dc4d0fb7086604930cdf689818fe88b5d42fba
Kuba Szpilowski <jakub.szpilowski@gmail.com>
2025-01-09 20:42:46 +0100

    Main loop simplified, add TODOs

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import mysql.connector
 from data.generation import Generator
-from utils.commons import (CONSOLE_PROMPT, MONGO_PROMPT, MYSQL_PROMPT, WRONG_COMMAND,
-                           NOT_IMPLEMENTED, CLOSE_APP, ELEMENTS_COUNT, BATCH_SIZE, INVALID_VALUE,
-                           BATCH_SIZE_INFO, CREATE_DATABASE, DEFAULT_SIZE, VALUE_TOO_LOW, VALUE_TOO_BIG,
-                           DATABASE_NOT_EXIST, DB_HOST, DB_USER, DB_PASS, DB_NAME, PATH_TO_STATS)
+from data.stats_collector import StatsCollector
+from database.queries.mongo.deleteMongo import DeleteMongo
+from database.queries.mongo.selectMongo import SelectMongo
+from database.queries.mongo.updateMongo import UpdateMongo
+from database.queries.mysql.deleteSQL import DeleteSQL
+from database.queries.mysql.selectSQL import SelectSQL
+from database.queries.mysql.updateSQL import UpdateSQL
+from pymongo import MongoClient
+from utils.commons import (CONSOLE_PROMPT, WRONG_COMMAND,
+                           NOT_IMPLEMENTED, CLOSE_APP,
+                           DB_HOST, DB_USER, DB_PASS, DB_NAME, NUMBER_OF_DATA, BATCH_SIZE)
 from utils.logger import Logger
 
 
+def print_help(command : str)-> None:
+    if command == "c":
+        print("  [c]   compare databases")
+        print("  params:")
+        print("    <n> number of iterations that will be performed")
+    elif command == "l":
+        print("  [l]   show program logs")
+    elif command == "q":
+        print("  [q]   quit application")
+    elif command == "h":
+        print("  [h]   display available commands")
+        print("  params:")
+        print("    <cmd> show command help")
+    else:
+        print(WRONG_COMMAND)
+
+
 def main_menu(cmp : bool) -> None:
-    print("\nDatabase comparison tool v1.0")
-    print("Usage: <command> ")
+    print("Database comparison tool v1.0")
+    print("Usage: <command> <optional params>")
     print("Available commands: ")
-    print("  [create]  create databases and generate data")
-    print("  [mongo]   perform operations in MongoDB database")
-    print("  [mysql]   perform operations in MySQL database")
+    print("  [c]       compare databases")
     if cmp:
         print("  [g]       create comparison graphs")
     print("  [l]       show program logs")
@@ -21,143 +43,298 @@ def main_menu(cmp : bool) -> None:
     print("  [q]       quit application")
 
 
-def database_menu(db: str, prompt : str) -> None:
-    print(f"Usage: {prompt}<command>")
-    print("Available commands: ")
-    print("  [s]   perform SELECT operations")
-    print("  [u]   perform UPDATE operations")
-    print("  [d]   perform DELETE operations")
-    print("  [h]   show help")
-    print("  [q]   quit {}".format(db))
+def progress_bar(msg : str, progress : int) -> None:
+    if progress == 50:
+        print(f"\r{msg}", end="")
+        print("")
+    else:
+        print(f"\r{msg} [" + "#" * progress + " " * (50 - progress) + "]", end="")
 
 
-def get_params(msg : str) -> int:
-    while True:
-        x = input(f"{msg}")
-        if x.lower() == "q":
-            return -1
-        if x.lower().strip() == "":
-            return DEFAULT_SIZE
-        try:
-            x = int(x)
-            if x < 1000:
-                raise Exception(VALUE_TOO_LOW)
-            if x > 1000000:
-                raise Exception(VALUE_TOO_BIG)
-            break
-        except ValueError:
-            print(f"{INVALID_VALUE}")
-        except Exception as e:
-            print(f"{INVALID_VALUE} - {str(e)}")
-    return x
-
-
-def create_databases() -> bool:
-    print(f"{CREATE_DATABASE}")
-    num_elements = get_params(ELEMENTS_COUNT)
-    if num_elements == -1:
-        return False
-    print(f"[INFO] Elements count: {num_elements}")
-    batch_size = get_params(BATCH_SIZE)
-    if batch_size == -1:
-        return False
-    print(f"[INFO] Batch size: {batch_size}")
-    if batch_size > num_elements:
-        print(f"{BATCH_SIZE_INFO}")
-        batch_size = DEFAULT_SIZE
+def insert_db(num_elements : int, batch_size : int) -> None:
     Generator.generate_files(num_elements, batch_size)
-    return True
 
 
-def update_db(db : str) -> None:
-    if db == "mysql":
-        connection = mysql.connector.connect(
+def update_db() -> None:
+    connection = mysql.connector.connect(
             host=DB_HOST,
             user=DB_USER,
             password=DB_PASS,
             database=DB_NAME
-        )
-    elif db == "mongo":
-        pass
+    )
+    client = MongoClient("mongodb://admin:password@localhost:27017/")
+    database = client[DB_NAME]
+    mysql_stats = StatsCollector("mysql", "UPDATE", 0, 0)
+    mongo_stats = StatsCollector("mongo", "UPDATE", 0, 0)
+
+    Logger.log("INFO", "Performing UPDATE operations")
+
+    # MYSQL
+    msg = "[UPDATE] Password and other fields when user is admin"
+    progress_bar(msg, 0)
+    result = UpdateSQL.update_password_and_other_fields_when_user_is_admin(connection)
+    mysql_stats.add_stats(result["total_time"], result["affected_rows"])
+    # MONGO
+    progress_bar(msg, 10)
+    result, mongo_time = UpdateMongo.update_password_and_other_fields_when_user_is_admin(database)
+    mongo_stats.add_stats(mongo_time, result.modified_count)
+
+    # MYSQL
+    msg = "[UPDATE] Reservation time"
+    progress_bar(msg, 10)
+    result = UpdateSQL.update_reservation_time(connection)
+    mysql_stats.add_stats(result["total_time"], result["affected_rows"])
+    # MONGO
+    progress_bar(msg, 20)
+    result, mongo_time = UpdateMongo.update_reservation_time(database)
+    mongo_stats.add_stats(mongo_time, result.modified_count)
+
+    # MYSQL
+    msg = "[UPDATE] Status when problem is older than"
+    progress_bar(msg, 20)
+    result = UpdateSQL.update_status_when_problem_is_older_than(connection)
+    mysql_stats.add_stats(result["total_time"], result["affected_rows"])
+    # MONGO
+    progress_bar(msg, 30)
+    result, mongo_time = UpdateMongo.update_status_when_problem_is_older_than(database)
+    mongo_stats.add_stats(mongo_time, result.modified_count)
+
+    # MYSQL
+    msg = "[UPDATE] One message"
+    progress_bar(msg, 30)
+    result = UpdateSQL.update_one_message(connection)
+    mysql_stats.add_stats(result["total_time"], result["affected_rows"])
+    # MONGO
+    progress_bar(msg, 40)
+    result, mongo_time = UpdateMongo.update_one_message(database)
+    mongo_stats.add_stats(mongo_time, result.modified_count)
+
+    connection.close()
+    Logger.save_stats(mysql_stats)
+    Logger.save_stats(mongo_stats)
+    progress_bar("[UPDATE] Completed", 50)
+    Logger.log("INFO", f"MySQL - data updated in {mysql_stats.total_time:.02f} seconds.")
+    Logger.log("INFO", f"Mongo - data updated in {mongo_stats.total_time:.02f} seconds.")
 
 
-def select_db(db : str) -> None:
-    if db == "mysql":
-        pass
-    elif db == "mongo":
-        pass
+def select_db() -> None:
+    connection = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS,
+            database=DB_NAME
+    )
+    client = MongoClient("mongodb://admin:password@localhost:27017/")
+    database = client[DB_NAME]
+    mysql_stats = StatsCollector("mysql", "SELECT", 0, 0)
+    mongo_stats = StatsCollector("mongo", "SELECT", 0, 0)
+    Logger.log("INFO", "Performing SELECT operations")
+
+    # MYSQL
+    msg = "[SELECT] Group reservation per device"
+    progress_bar(msg, 0)
+    response, elapsed = SelectSQL.group_reservation_per_device(connection)
+    mysql_stats.add_stats(elapsed, len(list(response)))
+    # MONGO
+    progress_bar(msg, 5)
+    response, elapsed = SelectMongo.group_reservation_per_device(database)
+    mongo_stats.add_stats(elapsed, len(list(response)))
+
+    # MYSQL
+    msg = "[SELECT] Count average reservation time per device"
+    progress_bar(msg, 5)
+    response, elapsed = SelectSQL.count_average_reservation_time_per_device(connection)
+    mysql_stats.add_stats(elapsed, len(list(response)))
+    # MONGO
+    progress_bar(msg, 10)
+    response, elapsed = SelectMongo.count_average_reservation_time_per_device(database)
+    mongo_stats.add_stats(elapsed, len(list(response)))
+
+    # MYSQL
+    msg = "[SELECT] Count number of admins"
+    progress_bar(msg, 10)
+    response, elapsed = SelectSQL.count_number_of_admins(connection)
+    mysql_stats.add_stats(elapsed, len(list(response)))
+    # MONGO
+    progress_bar(msg, 15)
+    response, elapsed = SelectMongo.count_number_of_admins(database)
+    mongo_stats.add_stats(elapsed, len(list(response)))
+
+    # MYSQL
+    msg = "[SELECT] Find problem by id"
+    progress_bar(msg, 15)
+    response, elapsed = SelectSQL.find_problem_by_id(connection, '000f28d2-eb2b-586a-949a-6439378f4d18')
+    mysql_stats.add_stats(elapsed, len(list(response)))
+    # MONGO
+    progress_bar(msg, 20)
+    response, elapsed = SelectMongo.find_problem_by_id(database, '674f8f8e8872d55fbc96ed06')
+    mongo_stats.add_stats(elapsed, len(list(response)))
+
+    # MYSQL
+    msg = "[SELECT] Find dormitory messages"
+    progress_bar(msg, 20)
+    response, elapsed = SelectSQL.find_dormitory_messages(connection, '00059349-f566-566d-a89d-c423576285d0')
+    mysql_stats.add_stats(elapsed, len(list(response)))
+    # MONGO
+    progress_bar(msg, 25)
+    response, elapsed = SelectMongo.find_dormitory_messages(database, '674f8f8b8872d55fbc96d8b9')
+    mongo_stats.add_stats(elapsed, len(list(response)))
+
+    # MYSQL
+    msg = "[SELECT] Find reservations earlier then concrete data"
+    progress_bar(msg, 25)
+    response, elapsed = SelectSQL.find_reservations_earlier_then_concrete_data(connection)
+    mysql_stats.add_stats(elapsed, len(list(response)))
+    # MONGO
+    progress_bar(msg, 30)
+    response, elapsed = SelectMongo.find_reservations_earlier_then_concrete_data(database)
+    mongo_stats.add_stats(elapsed, response)
+
+    # MYSQL
+    msg = "[SELECT] Find users"
+    progress_bar(msg, 30)
+    response, elapsed = SelectSQL.find_users(connection)
+    mysql_stats.add_stats(elapsed, len(list(response)))
+    # MONGO
+    progress_bar(msg, 35)
+    response, elapsed = SelectMongo.find_users(database)
+    mongo_stats.add_stats(elapsed, len(list(response)))
+
+    # MYSQL
+    msg = "[SELECT] Count problems by status"
+    progress_bar(msg, 35)
+    response, elapsed = SelectSQL.count_problems_by_status(connection)
+    mysql_stats.add_stats(elapsed, sum(r['count'] for r in response))
+    # MONGO
+    progress_bar(msg, 40)
+    response, elapsed = SelectMongo.count_problems_by_status(database)
+    mongo_stats.add_stats(elapsed, sum(r['count'] for r in list(response)))
+
+    # MYSQL
+    msg = "[SELECT] Count problems by status"
+    progress_bar(msg, 40)
+    response, elapsed = SelectSQL.group_messages_per_dormitory(connection)
+    mysql_stats.add_stats(elapsed, len(list(response)))
+    # MONGO
+    progress_bar(msg, 40)
+    response, elapsed = SelectMongo.group_messages_per_dormitory(database)
+    mongo_stats.add_stats(elapsed, len(list(response)))
+
+    connection.close()
+    progress_bar("[SELECT] Completed", 50)
+    Logger.save_stats(mysql_stats)
+    Logger.save_stats(mongo_stats)
+    Logger.log("INFO", f"MySQL - data selected in {mysql_stats.total_time:.02f} seconds.")
+    Logger.log("INFO", f"Mongo - data selected in {mongo_stats.total_time:.02f} seconds.")
 
 
-def delete_db(db : str) -> None:
-    if db == "mysql":
-        pass
-    elif db == "mongo":
-        pass
+def delete_db() -> None:
+    connection = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS,
+            database=DB_NAME
+    )
+    client = MongoClient("mongodb://admin:password@localhost:27017/")
+    database = client[DB_NAME]
+    mysql_stats = StatsCollector("mysql", "DELETE", 0, 0)
+    mongo_stats = StatsCollector("mongo", "DELETE", 0, 0)
+    Logger.log("INFO", "Performing DELETE operations")
+
+    # MYSQL
+    dorm_names = [f"Dormitory {i}" for i in range(1, 10)]
+    msg = "[DELETE] Many not in batch"
+    progress_bar(msg, 0)
+    elapsed = DeleteSQL.delete_many_not_in_batch(connection, dorm_names)
+    mysql_stats.add_stats(elapsed, len(dorm_names))
+    # MONGO
+    progress_bar(msg, 12)
+    elapsed = DeleteMongo.delete_many_not_in_batch(database, dorm_names)
+    mongo_stats.add_stats(elapsed, len(dorm_names))
+
+    # MYSQL
+    msg = "[DELETE] Many in batch"
+    progress_bar(msg, 12)
+    dorm_names = [f"Dormitory {i}" for i in range(10, 20)]
+    placeholders = ', '.join(['%s'] * len(dorm_names))
+    elapsed = DeleteSQL.delete_many_in_batch(connection, dorm_names, placeholders)
+    mysql_stats.add_stats(elapsed, len(dorm_names))
+    # MONGO
+    progress_bar(msg, 24)
+    elapsed = DeleteMongo.delete_many_in_batch(database, dorm_names)
+    mongo_stats.add_stats(elapsed, len(dorm_names))
+
+    # MYSQL
+    dorm_name = f"Dormitory 3979"
+    msg = "[DELETE] One dormitory"
+    progress_bar(msg, 24)
+    elapsed = DeleteSQL.delete_one(connection, dorm_name)
+    mongo_stats.add_stats(elapsed, 1)
+    # MONGO
+    progress_bar(msg, 36)
+    elapsed = DeleteMongo.delete_one(database, dorm_name)
+    mongo_stats.add_stats(elapsed, 1)
+
+    connection.close()
+    progress_bar("[DELETE] Completed", 50)
+    Logger.save_stats(mysql_stats)
+    Logger.save_stats(mongo_stats)
+    Logger.log("INFO", f"MySQL - data deleted in {mysql_stats.total_time:.02f} seconds.")
+    Logger.log("INFO", f"Mongo - data deleted in {mongo_stats.total_time:.02f} seconds.")
 
 
-def database_options(prompt : str, db_created : bool, db : str) -> None:
-    assert db == "mysql" or db == "mongo"
-    while True:
-        operation = input(prompt).lower()
-        if operation == "q":
-            return
-
-        elif operation == "s":
-            if not db_created:
-                print(DATABASE_NOT_EXIST(prompt))
-                return
-            # TODO : SELECT
-            print(NOT_IMPLEMENTED(prompt))
-
-        elif operation == "u":
-            if not db_created:
-                print(DATABASE_NOT_EXIST(prompt))
-                return
-            update_db(db)
-
-        elif operation == "d":
-            if not db_created:
-                print(DATABASE_NOT_EXIST(prompt))
-                return
-
-            # TODO : DELETE
-            print(NOT_IMPLEMENTED(prompt))
-
-        elif operation == "h":
-            database_menu(db, prompt)
-
-        else:
-            print(WRONG_COMMAND(prompt))
-
-
-def compare() -> bool:
-    return False
-
+def compare(iters : int) -> bool:
+    Logger.log("INFO", "Comparing databases - start")
+    for index, num_elements in enumerate(NUMBER_OF_DATA):
+        batch_size = BATCH_SIZE[index]
+        Logger.log("INFO", f"Number of data: {num_elements}")
+        for i in range(iters):
+            Logger.log("INFO", f"Iteration {i + 1} / {iters}")
+            insert_db(num_elements, batch_size)
+            select_db()
+            update_db()
+            delete_db()
+    Logger.log("INFO", "Comparing databases - finished")
+    return True
 
 if __name__ == '__main__':
     compared = False
+    print()
     main_menu(compared)
     while True:
         op = input(CONSOLE_PROMPT).lower()
+        tmp = op.split(" ")
+        if len(tmp) == 2:
+            op, opt = tmp[0].strip(), tmp[1].strip()
+        else:
+            op = tmp[0].strip()
+            opt = ""
         if op == "q":
-            print(CLOSE_APP(CONSOLE_PROMPT))
+            print(CLOSE_APP)
             break
         if op == "c":
-            compared = compare()
-        elif op == "create":
-            created = create_databases()
-        elif op == "mysql":
-            database_options(MYSQL_PROMPT, created, "mysql")
-        elif op == "mongo":
-            database_options(MONGO_PROMPT, created, "mongo")
+            try:
+                opt = int(opt)
+            except ValueError:
+                Logger.log("INFO", "Using default value (1).")
+                opt = 1
+            compared = compare(opt)
         elif op == "g":
             if not compared:
                 continue
-            # TODO
-            print(NOT_IMPLEMENTED(CONSOLE_PROMPT))
+            print(NOT_IMPLEMENTED)
         elif op == "l":
             Logger.print_logs()
         elif op == "h":
-            main_menu(compared)
+            if opt == "":
+                main_menu(compared)
+            else:
+                print_help(opt)
         else:
-            print(WRONG_COMMAND(CONSOLE_PROMPT))
+            print(WRONG_COMMAND)
+
+# TODO: - DELETE - można by przetestować większe zakresy ?
+#       - Wykresy
+#       - Przenieść operacje z main.py do osobnych plików -> będzie czytelniej
+#       - Pasek postępu ustawiany w zależności od długości stringów
+#       - Można dodać pasek postępu do INSERTów
