cba9bb1e5c3724088e198619562fdc9b695f8577
Kuba Szpilowski <jakub.szpilowski@gmail.com>
2025-01-09 20:40:20 +0100

    Simplify main loop - delete unused CONSTs

--- utils/commons.py
+++ utils/commons.py
@@ -1,4 +1,5 @@
-number_of_data = [1000, 10000, 100000, 1000000]
+NUMBER_OF_DATA = [1000, 10000, 100000, 1000000]
+BATCH_SIZE = [1000, 5000, 20000, 100000]
 DB_HOST = "localhost"
 DB_USER = "user"
 DB_PASS = "password"
@@ -8,19 +9,7 @@ PATH_TO_LOGS = 'output/logs/'
 PATH_TO_STATS = 'output/stats/'
 
 CONSOLE_PROMPT = "$console> "
-MYSQL_PROMPT = "$mysql> "
-MONGO_PROMPT = "$mongo> "
 
-WRONG_COMMAND = lambda p : f"{p}[ERROR] Unknown command"
-NOT_IMPLEMENTED = lambda p : f"{p}[ERROR] Command not implemented"
-CLOSE_APP = lambda p : f"{p}[INFO] Application is closing..."
-DATABASE_NOT_EXIST = lambda p : f"{p}[ERROR] Database does not exist. Exiting..."
-
-CREATE_DATABASE = "Database creation ([q] to quit)"
-DEFAULT_SIZE = 1000
-ELEMENTS_COUNT = f"Number of elements to generate in tables (default {DEFAULT_SIZE}): "
-BATCH_SIZE = f"Enter size of batch (default {DEFAULT_SIZE}): "
-INVALID_VALUE = "[ERROR] Invalid value"
-BATCH_SIZE_INFO = "[INFO] Batch size > number of elements -> using default value"
-VALUE_TOO_LOW = "value too small (must be greater than 1000)"
-VALUE_TOO_BIG = "value too big (must be less than 1000000)"
+WRONG_COMMAND = f"{CONSOLE_PROMPT}[ERROR] Unknown command"
+NOT_IMPLEMENTED = f"{CONSOLE_PROMPT}[ERROR] Command not implemented"
+CLOSE_APP = f"{CONSOLE_PROMPT}[INFO] Application is closing..."
